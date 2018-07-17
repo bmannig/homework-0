672f5c4b7b085728f99effa6806053695ffa53b3
bmannig <bmannig@gmx.net>
2018-07-17 12:40:58 +0200

    funzt ssh?

--- code.R
+++ code.R
@@ -1,2 +1,8 @@
 library(tidyverse)
 #.gitignore als Endung, wenn wir nicht wollen, dass die auf github hochgeladen werden
+
+# I need a change...
+a <- c(10:100)
+
+# and anohther to test something on github
+b <- c(1:9)
